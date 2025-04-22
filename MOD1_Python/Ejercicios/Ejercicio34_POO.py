0a0d0689fa42a3efbbe59bd148113cd7ef2bc8a4
JonatanMendez17 <jonatanmendez17@hotmail.com>
2025-04-22 07:57:09 -0300

    Nuevos ejercicios de clase 7

---
+++ MOD1_Python/Ejercicios/Ejercicio34_POO.py
@@ -0,0 +1,10 @@
+# Ejercicio 34:
+
+# Disena una clase llamada CuentaBancaria con los atributos:
+    # titular: nombre del dueno de la cuenta.
+    # saldo: comienza en 0 si no se indica lo contrario.
+
+# La clase debe tener los siguientes metodos:
+    # depositar(monto): suma el monto al saldo.
+    # retirar(monto): resta el monto si hay suficiente saldo; si no, muestra un mensaje de error.
+    # mostrar_saldo(): imprime el saldo actual y el nombre del titular.
