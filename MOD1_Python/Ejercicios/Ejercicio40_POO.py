9551d15922a3ab9ba83e1ebe8b919b215ce441f0
JonatanMendez17 <129536099+JonatanMendez17@users.noreply.github.com>
2025-04-26 11:50:13 -0300

    Se agregan dos ejercicios mas sobre POO

---
+++ MOD1_Python/Ejercicios/Ejercicio40_POO.py
@@ -0,0 +1,8 @@
+# Ejercicio 40: 
+
+# Crea una clase llamada Libro con los siguientes atributos:
+    # titulo (nombre del libro)
+    # autor (nombre del autor del libro)
+
+# La clase debe tener un método:
+    # mostrar_info() que imprima un mensaje con el formato:"Título: [titulo], Autor: [autor]"
---
+++ MOD1_Python/Ejercicios/Ejercicio41_POO.py
@@ -0,0 +1,16 @@
+# Ejercicio 41:
+
+# Crea una clase llamada Producto con los siguientes atributos:
+    # Atributo privado: __precio (precio del producto).
+    # Atributo público: nombre (nombre del producto).
+
+# La clase debe tener los siguientes métodos:
+    # Método actualizar_precio(nuevo_precio): Permite actualizar el precio del producto.
+    # Método mostrar_info(): Muestra el nombre del producto y su precio.
+    # Método obtener_precio(): Retorna el precio actual del producto (esto es para acceder al atributo privado __precio de manera controlada).
+
+# Uso:
+    # Crea un objeto de la clase Producto con un nombre y un precio inicial.
+    # Actualiza el precio a un nuevo valor.
+    # Muestra la información del producto después de la actualización del precio. 
+
