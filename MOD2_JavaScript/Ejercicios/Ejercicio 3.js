de4683420ba5dad1761923f8475b66b59eb84bd5
JonatanMendez17 <129536099+JonatanMendez17@users.noreply.github.com>
2025-05-20 14:33:42 -0300

    Comienzo de modulo 2

---
+++ MOD2_JavaScript/Ejercicios/Ejercicio 3.js
@@ -0,0 +1,7 @@
+// Ejercicio 3: Suma de dos números
+// Objetivo: Introducir la conversión de tipos de datos y la suma.
+
+// Instrucciones:
+// Pedí dos números con prompt.
+// Convertí las entradas a números.
+// Sumalos y mostrálos en consola.
